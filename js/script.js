eb49c678712d233aff6b8d812fb8cdb86f56dab7
SG0032 <salahguirnaoui10@gmail.com>
2023-12-25 22:11:04 +0100

    fist commit

---
+++ js/script.js
@@ -0,0 +1,73 @@
+let menu = document.querySelector('#menu-btn');
+let navbarLinks = document.querySelector('.header .navbar .links');
+
+menu.onclick = () =>{
+   menu.classList.toggle('fa-times');
+   navbarLinks.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+   menu.classList.remove('fa-times');
+   navbarLinks.classList.remove('active');
+
+   if(window.scrollY > 60){
+      document.querySelector('.header .navbar').classList.add('active');
+   }else{
+      document.querySelector('.header .navbar').classList.remove('active');
+   }
+}
+
+let videoBtn = document.querySelectorAll('.vid-btn');
+
+
+videoBtn.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    });
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".brand-slider", {
+    spaceBetween: 20,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        450: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        991: {
+          slidesPerView: 4,
+        },
+        1200: {
+          slidesPerView: 5,
+        },
+      },
+});
